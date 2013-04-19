58730bbf3a7f9f9857f00b5af215183aca3aa9f9
Tim Pease <tim.pease@gmail.com>
2013-04-19 12:08:33 -0600

    we'll use the revision number instead for this release

--- oa-enterprise/lib/omniauth/version.rb
+++ oa-enterprise/lib/omniauth/version.rb
@@ -7,7 +7,7 @@ module OmniAuth
       MINOR = 3
     end
     unless defined?(::OmniAuth::Version::PATCH)
-      PATCH = 3
+      PATCH = 2
     end
     unless defined?(::OmniAuth::Version::PRE)
       PRE   = nil
